07ada61997f476ba600fbbd327560f2fcd154fa3
harishsingh-01 <harishchaudhary790@gmail.com>
2025-12-29 18:21:14 +0530

    cors updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS Configuration
 const corsOptions = {
-  origin: 'https://harish-projects.vercel.app',
+  origin: 'https://harish-mern-projects.vercel.app'||'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
